e1ca80f50dde7e68950330b2678821470b498f58
Tom Lant <lampholder@gmail.com>
2017-08-01 16:46:18 +0100

    Object orientation???

--- server/app.js
+++ server/app.js
@@ -14,27 +14,97 @@
 
     /* I don't know how to program in node, so let's try and keep it simple. */
 
-    // So far the sessionManager's being db-backed is almost entirely valueless :(
-    var sessionManager = {
-        db: function() {
+    function SessionManager() {
+
+        function Session(id, token, width, height) {
+            console.log('creating new session obj');
+            this.id = function() { return id; };
+            this.token = function() { return token; };
+            this.width = function() { return width; };
+            this.height = function() { return height; };
+
+            var sendSubscriberCount = function() {
+                if (this.publisher() != null &&
+                    this.publisher().readyState === WebSocket.OPEN) {
+                    this.publisher().send(JSON.stringify({type: 'viewcount',
+                                                          body: this.subscribers().length}));
+                }
+            };
+
+            var publisher = null;
+            this.publisher = function(pub, token) {
+                if (pub === null) {
+                    return publisher;
+                }
+                else if (token === this.token()) {
+                     publisher = pub;
+                     sendSubscriberCount();
+                }
+            };
+
+            var subscribers = [];
+            this.subscribe = function(subscriber) {
+                if (!(subscriber in subscribers)) {
+                    subcribers.push(subscriber);
+                    sendSubscriberCount();
+                }
+            };
+            this.unsubscribe = function(subscriber) {
+                if (subscriber in subscribers) {
+                    subscribers = subscribers.filter(function(s) { return s.uuid !== websocket.uuid; });
+                    sendSubscriberCount();
+                }
+            };
+
+            this.broadcast = function broadcast(data) {
+                var string_data = JSON.stringify(data);
+                subscribers.forEach(function each(subscriber) {
+                    if (subscriber.readyState === WebSocket.OPEN) {
+                        subscriber.send(string_data);
+                    }
+                });
+            };
+
+        }
+
+        var sessions = {};
+        this.getSessions = function() {
+            return sessions;
+        };
+
+        this.getSession = function(sessionId) {
+            if (sessionId in sessions) {
+                return sessions[sessionId];
+            }
+            else {
+                return null;
+            }
+        };
+
+        var db = function() {
+            /* Establishes a connection to the database; installs the schema if necessary */
             var connection = new sqlite3.Database('sessions.db');
             connection.serialize(function() {
-                connection.run('create table if not exists sessions (sessionId text not null, width integer not null, height integer not null)');
+                connection.run('create table if not exists sessions (sessionId text not null, token text not null, width integer not null, height integer not null)');
             });
             return connection;
-        }(),
-        registerSession: function(width, height) {
-            var self = this;
+        }();
+
+        this.registerSession = function(width, height) {
             return new Promise(function(fullfil, reject) {
                 (function myself() {
                     var sessionId = RandomWords(1)[0];
-                    self.db.get('select count(sessionId) as total from sessions where sessionId = ?',
+                    console.log('Checking availability for \'' + sessionId + '\'');
+                    db.get('select count(sessionId) as total from sessions where sessionId = ?',
                                 [sessionId],
                                 function(err, row) {
                                     if (row.total == 0) {
-                                        var stmt = self.db.prepare('insert into sessions (sessionId, width, height) values (?, ?, ?)');
+                                        var stmt = db.prepare('insert into sessions (sessionId, width, height) values (?, ?, ?)');
                                         stmt.run(sessionId, width, height);
-                                        fullfil(sessionId);
+                                        var session = new Session(sessionId, 'abc123', width, height);
+                                        sessions[session.id] = session;
+                                        console.log('session created');
+                                        fullfil(session);
                                     }
                                     else {
                                         console.log(sessionId + ' was no good; trying another.');
@@ -43,15 +113,10 @@
                                 });
                 })();
             });
-        },
-        destroySession: function(sessionId) {
-            var self = this;
-            return new Promise(function(fullfil, reject) {
-                var query = self.db.query('delete from sessions where id = ?' [sessionId]);
-                fullfil();
-            });
-        }
-    };
+        };
+    }
+
+    var sessionManager = new SessionManager();
 
     var connectionManager = {
         publishers: {},
@@ -72,16 +137,16 @@
     };
 
     app.get('/init', function init(request, response) {
+        //TODO: need to have an optional token in here
         sessionManager.registerSession(request.query.width, request.query.height).then(
-            function(sessionId) {
-                console.log(sessionId);
-                response.send({session_id: sessionId,
-                               width: request.query.width,
-                               height: request.query.height});
-                //subscribers[sessionId] = [];
-                //publishers_termsizes[sessionId] = {'width': 0, 'height': 0};
-                app.use('/' + sessionId, function(req, res) {
-                    var dimensions = publishers_termsizes[sessionId];
+            function(session) {
+                response.send({id: session.id(),
+                               token: session.token(),
+                               width: session.width(),
+                               height: session.height()
+                });
+                app.use('/' + session.id(), function(req, res) {
+                    var dimensions = publishers_termsizes[session.id()];
                     res.render('index', {'height': dimensions.height, 'width': dimensions.width}); 
                 });
             }
@@ -91,6 +156,7 @@
 
     app.use('/static', express.static('static'));
 
+
     app.get('/', function(req, res) {
         res.send('<h1>Toml\'s super-good termcast thinger!</h1>' +
                  '<p>Go <a href="https://github.com/lampholder/termcast/tree/master/cli">to the github page' +
@@ -98,25 +164,13 @@
                  ' your terminal session with the internet intuitively and greatly for fun and profit!</p>');
     });
 
-    const httpServer = http.createServer(app);
-    const websocketServer = new WebSocket.Server({ httpServer });
+    const server = http.createServer(app);
+    const wss = new WebSocket.Server({ server });
 
-    // Broadcast to everyone in a session.
-    websocketServer.broadcast = function broadcast(sessionId, message) {
-        var clients = connectionManager.subscribers[sessionId];
-        clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    };
-
-    websocketServer.on('connection', function connection(websocket, request) {
+    wss.on('connection', function connection(websocket, request) {
         function guid() {
             function s4() {
-                return Math.floor((1 + Math.random()) * 0x10000)
-                       .toString(16)
-                       .substring(1);
+                return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
             }
             return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
                    s4() + '-' + s4() + s4() + s4();
@@ -127,8 +181,7 @@
 
         websocket.on('message', function incoming(msg) {
             var message = JSON.parse(msg);
-            var sessionId = location.path.substr(1);
-            var session = subscribers[sessionId];
+            var session = sessionManager.getSession(location.path.substr(1));
 
             if (message.type != 'stream') {
                 console.log(message.type + ': ' + message.body);
@@ -136,35 +189,18 @@
 
             switch(message.type) {
                 case 'registerPublisher':
-                    connectionManager.registerPublisher(sessionId, websocket);
-                    publishers[sessionId].send(JSON.stringify({'type': 'viewcount', 'msg': session.length}));
-                    console.log('Publisher registered for stream "' + sessionId + '"');
+                    session.publisher(websocket, 'abc123');
+                    session.publisher().send(JSON.stringify({'type': 'viewcount', 'msg': session.length}));
+                    console.log('Publisher registered for stream "' + session.id + '"');
                     break;
                 case 'stream':
-                    websocketServer.broadcast(sessionId, JSON.stringify(message));
+                    session.broadcast(message);
                     break;
                 case 'registerSubscriber':
-                    connectionManager.subscribe(sessionId, websocket);
-
-                    // Notify the publisher that they've got a new subscriber (this doesn't belong here)
-                    if (sessionId in connectionManager.publishers &&
-                        connectionManager.publishers[sessionId].readyState === WebSocket.OPEN) {
-                        connectionManager.publisers[sessionId].send(
-                                JSON.stringify({type: 'viewcount',
-                                                body: connectionManager.subscribers[sessionId].length}));
-                    }
-
+                    session.subscribe(websocket);
                     websocket.on('close', function() {
-                        console.log('Subscriber unregistered from stream "' + sessionId);
-                        connectionManager.unsubscribe(sessionId, websocket);
-
-                        // Notify the publisher that they've lost a subscriber (this doesn't belong here)
-                        if (sessionId in connectionManager.publishers &&
-                            connectionManager.publishers[sessionId].readyState === WebSocket.OPEN) {
-                            connectionManager.publisers[sessionId].send(
-                                    JSON.stringify({type: 'viewcount',
-                                                    body: connectionManager.subscribers[sessionId].length}));
-                        }
+                        console.log('Subscriber unregistered from stream "' + session.id);
+                        session.unsubscribe(websocket);
                     });
                     break;
             }
@@ -172,8 +208,8 @@
     });
 
 
-    httpServer.listen(80, function listening() {
-      console.log('Listening on %d', httpServer.address().port);
+    server.listen(80, function listening() {
+      console.log('Listening on %d', server.address().port);
     });
 
 }());
